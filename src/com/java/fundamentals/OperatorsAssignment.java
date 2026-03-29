9044f069fa8336d4cad11771799f92ce90170d26
Saddala Yogesh <saddalayogesh@gmail.com>
2026-03-29 23:12:16 +0530

    Update OperatorsAssignment.java

--- src/com/java/fundamentals/OperatorsAssignment.java
+++ src/com/java/fundamentals/OperatorsAssignment.java
@@ -4,18 +4,46 @@ import java.util.Scanner;
 
 public class OperatorsAssignment {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter id");
-        byte id = scanner.nextByte();
-        System.out.println("Enter productName");
-        String productName = scanner.next();
-        System.out.println("Enter maxRetailPrice");
-        int maxRetailPrice = scanner.nextInt();
-        System.out.println("Enter discountPercentage");
-        float discountPercentage = scanner.nextFloat();
-        double stateTax = (double)2.5F;
-        double centralTax = (double)2.5F;
-        double discountAmount = (double)(((float)maxRetailPrice + discountPercentage) / 100.0F);
-        double finalPrice = (double)maxRetailPrice - discountAmount;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Product Price ");
+        double productPrice = sc.nextDouble();
+        System.out.println(" Enter Membership Type");
+        char membershipType = sc.next().charAt(0);
+        System.out.println("Enter quantity");
+        int quantity = sc.nextInt();
+
+        double discount = 0;
+        double shipping = 0;
+
+        if (membershipType == 'S') {
+            discount = 0.10;
+            shipping = 50;
+        } else if (membershipType == 'G') {
+            discount = 0.15;
+            shipping = 20;
+        } else if (membershipType == 'D') {
+            discount = 0.20;
+            shipping = 0;
+        } else {
+            discount = 0;
+            shipping = 100;
+        }
+        double discountAmount = productPrice * discount;
+        double discountedPrice = productPrice - discountAmount;
+        double taxAmount = discountedPrice * 0.05;
+
+        double finalPrice = discountedPrice + taxAmount + shipping;
+        double totalBill = finalPrice * quantity;
+        double totalSavings = discountAmount * quantity;
+
+
+        System.out.println("Product Price: " + productPrice);
+        System.out.println("Discount Amount: " + discountAmount);
+        System.out.println("Final Price per Product: " + finalPrice);
+        System.out.println("Total Bill: " + totalBill);
+        System.out.println("Total Savings: " + totalSavings);
+
+        sc.close();
     }
 }
+
